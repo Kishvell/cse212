3704509a1a535d8d4cb89afd1703ea1332967c29
Santiago Benjamín Irigoyen <irigoyensantiago623@gmail.com>
2025-07-18 20:39:32 -0300

    We finally fixed it!!

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,12 +8,14 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        double[] result = new double[count];
 
-        return []; // replace this return statement with your own
+        for (int i = 0; i < count; i++)
+        {
+            result[i] = start * (i + 1);
+        }
+
+        return result;
     }
 
     /// <summary>
